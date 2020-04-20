d2484732fc8b7e1dc5324cc4365c780e8717b41e
Marissa <1995marissapsy@live.com>
2020-04-20 11:06:25 -0400

    check

--- clean_data.R
+++ clean_data.R
@@ -1,10 +1,9 @@
 #!/usr/local/bin/Rscript
-#setwd("~/Documents/GitHub/covid19-Rcode/")
+setwd("~/Documents/GitHub/covid19-Rcode/")
 
 library(data.table)
 library(dplyr)
 library(tidyr)
-
 
 # read in data
 intl_confirmed = fread("data/time_series_covid19_confirmed_global.csv", stringsAsFactors = F)
@@ -12,20 +11,42 @@ intl_confirmed = intl_confirmed %>% select(-Lat, -Long)
 intl_confirmed = gather(intl_confirmed, Date, Num_Confirmed, 3:ncol(intl_confirmed), factor_key=TRUE)
 colnames(intl_confirmed) = c("State", "Country", "Date", "Num_Confirmed")
 
+intl_deaths = fread("data/time_series_covid19_deaths_global.csv", stringsAsFactors = F)
+intl_deaths = intl_deaths %>% select(-Lat, -Long)
+intl_deaths = gather(intl_deaths, Date, Num_Deaths, 3:ncol(intl_deaths), factor_key=TRUE)
+colnames(intl_deaths) = c("State", "Country", "Date", "Num_Deaths")
+
 us_confirmed = fread("data/time_series_covid19_confirmed_US.csv", stringsAsFactors = F)
 us_confirmed = us_confirmed %>% select(-UID, -iso2, -iso3, -code3, -FIPS, -Admin2, -Combined_Key, -Lat, -Long_)
 us_confirmed = gather(us_confirmed, Date, Num_Confirmed, 3:ncol(us_confirmed), factor_key=TRUE)
 colnames(us_confirmed) = c("State", "Country", "Date", "Num_Confirmed")
 us_confirmed = us_confirmed %>% group_by(State, Country, Date) %>% summarise(Num_Confirmed = sum(Num_Confirmed)) %>% ungroup()
 
+us_deaths = fread("data/time_series_covid19_deaths_US.csv", stringsAsFactors = F)
+us_deaths = us_deaths %>% select(-UID, -iso2, -iso3, -code3, -FIPS, -Admin2, -Combined_Key, -Lat, -Long_)
+us_deaths = gather(us_deaths, Date, Num_Deaths, 3:ncol(us_deaths), factor_key=TRUE)
+colnames(us_deaths) = c("State", "Country", "Date", "Num_Deaths")
+us_deaths = us_deaths %>% group_by(State, Country, Date) %>% summarise(Num_Deaths = sum(Num_Deaths)) %>% ungroup() %>% filter(Date != "Population")
+
 
 # combine data and clean a little
 confirmed = rbind(intl_confirmed %>% filter(Country != "US"), us_confirmed)
+deaths = rbind(intl_deaths %>% filter(Country != "US"), us_deaths)
+
 confirmed = confirmed %>% mutate(Date = as.Date(Date, format = "%m/%d/%y"))
+deaths = deaths %>% mutate(Date = as.Date(Date, format = "%m/%d/%y"))
 
 # remap hong kong and rename US
 confirmed = confirmed %>%
   mutate(Country = ifelse(State == "Hong Kong", "Hong Kong", Country),
+         Country = ifelse(Country == "Taiwan*", "Taiwan", Country),
+         Country = ifelse(Country == "US", "United States", Country))
+
+deaths = deaths %>%
+  mutate(Country = ifelse(State == "Hong Kong", "Hong Kong", Country),
+         Country = ifelse(Country == "Taiwan*", "Taiwan", Country),
          Country = ifelse(Country == "US", "United States", Country))
 
 fwrite(confirmed, '../covid19-Django/trends/data/confirmed_cases.csv')
+fwrite(deaths, '../covid19-Django/trends/data/num_deaths.csv')
+
--- daily_data_pull.sh
+++ daily_data_pull.sh
@@ -9,11 +9,16 @@ cp ../COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_
 
 cp ../COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv data/time_series_covid19_confirmed_US.csv
 
+cp ../COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv data/time_series_covid19_deaths_global.csv
+
+cp ../COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv data/time_series_covid19_deaths_US.csv
+
 Rscript clean_data.R
 
 cd ../covid19-Django
 
 git add trends/data/confirmed_cases.csv
+git add trends/data/num_death.csv
 git commit -m "refresh with today's data"
 git push origin master
 git push heroku master
